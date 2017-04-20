5502e9f13c1e49239c26a82225f049227c3618e1
yuhao.ju <yuhao.ju@qunar.com>
2017-04-20 16:38:29 +0800

    feat: 兼容 webpack 2

--- index.js
+++ index.js
@@ -2,30 +2,59 @@
 
 exports.config = function (options, cwd) {
     var baseConfig = this.config;
-    baseConfig.module.loaders = baseConfig.module.loaders.concat([
-        {
-            test: /\.vue$/,
-            loader: 'vue-loader'
-        }, {
-            test: /\.js$/,
-            loader: 'babel-loader',
-            query: {
-                presets: [
-                    'es2015',
-                    'es2017',
-                    'stage-0',
-                    'stage-1',
-                    'stage-2',
-                ]
-            },
-            exclude: /node_modules/
+    var vueQuery = 'babel-loader?presets[]=es2015,presets[]=es2017,presets[]=stage-0,presets[]=stage-1,presets[]=stage-2';
+    var babelPresets = [
+        'es2015',
+        'es2017',
+        'stage-0',
+        'stage-1',
+        'stage-2',
+    ];
+
+    if(!this.webpack.version || this.webpack.version < 2) {
+        baseConfig.module.loaders = baseConfig.module.loaders.concat([
+            {
+                test: /\.vue$/,
+                loader: require.resolve('vue-loader')
+            }, {
+                test: /\.js$/,
+                loader: require.resolve('babel-loader'),
+                query: {
+                    presets: babelPresets
+                },
+                exclude: /node_modules/
+            }
+        ])
+        baseConfig.vue = {
+            loaders: {
+                js: vueQuery
+            }
         }
-    ])
-    baseConfig.vue = {
-        loaders: {
-            js: "babel-loader?presets[]=es2015,presets[]=es2017,presets[]=stage-0,presets[]=stage-1,presets[]=stage-2"
-        }
+    } else {
+        baseConfig.module.loaders = baseConfig.module.loaders.concat([
+            {
+                test: /\.vue$/,
+                use: [{
+                    loader: require.resolve('vue-loader'),
+                    options: {
+                        loaders: {
+                            js: vueQuery
+                        }
+                    }
+                }]
+            }, {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: require.resolve('babel-loader'),
+                    query: {
+                        presets:babelPresets
+                    }
+                }]
+            }
+        ])
     }
+
     baseConfig.devtool = '#eval-source-map'
     if (this.env === 'prd') {
         baseConfig.plugins = baseConfig.plugins.concat([
